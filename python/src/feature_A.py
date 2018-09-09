ab5d98a2027345e6c7640c0f57eecdd2bb4bea8f
cocarica53120 <36820588+cocarica53120@users.noreply.github.com>
2018-09-09 23:32:21 +0200

    Update feature_A.py

--- python/src/feature_A.py
+++ python/src/feature_A.py
@@ -4,4 +4,5 @@ print('feature_A Creation');
 print('feature_A First modification');
 print('feature_A Second modification as required by cocarica53120 in Pull Request');
 print('feature_A Third modification as required by cocarica53120 in another Comment of Pull Request named "Feature_A Creation"');
+print('feature_A Has been rebased on master');
 
