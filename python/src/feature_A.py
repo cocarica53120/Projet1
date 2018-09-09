871b2875b268c43f24618e938432304f19190ab4
cocarica53120 <alain.collet2@gmail.com>
2018-09-09 23:19:04 +0200

    Added Modif_3 as required in Pull Request

--- python/src/feature_A.py
+++ python/src/feature_A.py
@@ -3,4 +3,5 @@
 print('feature_A Creation');
 print('feature_A First modification');
 print('feature_A Second modification as required by cocarica53120 in Pull Request');
+print('feature_A Third modification as required by cocarica53120 in another Comment of Pull Request named "Feature_A Creation"');
 
